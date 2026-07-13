ece5a993992e33538e358792515b556bc7da69de
MOMO-4747 <momoxmd7@gmail.com>
2026-07-14 01:33:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+// === MOMO XMD CONFIG ===
+global.botName = 'MOMO XMD'
+global.owner = ['255760298574']
+global.ownerNumber = '255760298574'
+global.prefix = '.'
+global.sessionName = 'MOMO-XMD'
+global.footer = '© 2026 MOMO XMD'
+// === END CONFIG ===
