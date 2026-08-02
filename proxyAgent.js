097a53c903ab90a920938ab4cda68a2f0454f6d1
root <root@srv584446945.host>
2026-08-02 22:44:00 +0000

    Use Tor proxy for residential-like IPs

--- proxyAgent.js
+++ proxyAgent.js
@@ -1,28 +1,18 @@
-const { HttpsProxyAgent } = require('https-proxy-agent');
+const { SocksProxyAgent } = require('socks-proxy-agent');
 
-const PROXIES = [
-    'http://xclayddg:us4xfz7g8vto@31.59.20.176:6754',
-    'http://xclayddg:us4xfz7g8vto@31.56.127.193:7684',
-    'http://xclayddg:us4xfz7g8vto@45.38.107.97:6014',
-    'http://xclayddg:us4xfz7g8vto@198.105.121.200:6462',
-    'http://xclayddg:us4xfz7g8vto@64.137.96.74:6641',
-    'http://xclayddg:us4xfz7g8vto@198.23.243.226:6361',
-    'http://xclayddg:us4xfz7g8vto@38.154.185.97:6370',
-    'http://xclayddg:us4xfz7g8vto@84.247.60.125:6095',
-    'http://xclayddg:us4xfz7g8vto@142.111.67.146:5611',
-    'http://xclayddg:us4xfz7g8vto@191.96.254.138:6185'
-];
-
-let currentIndex = 0;
+let currentProxy = null;
 
 function getNextProxyAgent() {
-    const url = PROXIES[currentIndex % PROXIES.length];
-    currentIndex++;
     try {
-        return new HttpsProxyAgent(url);
+        // Use Tor SOCKS5 proxy (always available, residential-like IPs)
+        if (!currentProxy) {
+            currentProxy = new SocksProxyAgent('socks5://127.0.0.1:9050');
+        }
+        return currentProxy;
     } catch (e) {
+        console.error('Tor proxy error:', e.message);
         return null;
     }
 }
 
-module.exports = { getNextProxyAgent, PROXIES };
+module.exports = { getNextProxyAgent, PROXIES: ['tor://127.0.0.1:9050'] };
